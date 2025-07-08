aa1ad4b52b42a4f724bd36bbe9ce2bd7aa398f26
bheutschi <bheutschi@gmail.com>
2025-07-08 13:42:50 +0200

    feat: remove the wrong back and add the good one

--- back/resources/views/livewire/combobox/index.blade.php
+++
@@ -1,8 +0,0 @@
-<div>
-    <input type="text" class="input" placeholder="Bénéficiaire" list="users" wire:model.live="search" name="{{ $display }}"/>
-    <datalist id="users">
-        @foreach ($results as $result)
-        <option wire:key="{{ $result->id }}" value="{{$result->email}}"></option>
-        @endforeach
-    </datalist>
-</div>
--- back/resources/views/test.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-@section('title', 'Test')
-@section('content')
-<div class="container mx-auto mt-8">
-    <livewire:toggle-input/>
-</div>
-@endsection
